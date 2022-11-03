d96b8959ba5778b13c2be31a3cf5f08a1ad34007
Hanashi <hanashi@hanashi.eu>
2022-11-03 15:00:51 +0100

    First commit

---
+++ files/lib/system/option/user/FediverseUserOptionOutput.class.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace wcf\system\option\user;
+
+use Laminas\Diactoros\Uri;
+use wcf\data\user\option\UserOption;
+use wcf\data\user\User;
+use wcf\util\StringUtil;
+
+final class FediverseUserOptionOutput implements IUserOptionOutput
+{
+    /**
+     * @inheritDoc
+     */
+    public function getOutput(User $user, UserOption $option, $value)
+    {
+        if (empty($value)) {
+            return '';
+        }
+
+        $values = \explode('@', $value);
+        if (\count($values) !== 3) {
+            return '';
+        }
+
+        $url = (new Uri())
+            ->withScheme('https')
+            ->withHost($values[2])
+            ->withPath('@' . $values[1]);
+
+        return StringUtil::getAnchorTag((string)$url, $value);
+    }
+}
